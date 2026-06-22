66c57d50cfaace7635ee38b11726da48f960ca2c
ayanb22 <ayanb2227@gmail.com>
2026-06-22 16:27:26 +0530

    Added Deposit Method

--- Bank System/account.py
+++ Bank System/account.py
@@ -5,3 +5,16 @@ class Account:
     def __init__(self, name, amount):
         self.name = name
         self.amount = amount
+
+    def deposit(self):
+        amount = self.amount
+        while True:
+            if amount > 0:               
+                self.balance += amount
+                print(f"Amount Rs.{amount} is credited")
+                print(f"Your Current Balance is {self.balance}")
+                print("Deposit Successful")
+                break
+            else:
+                print("Try again that is an invalid amount....")
+                amount = int(input("Enter the amount you want to deposit : "))
